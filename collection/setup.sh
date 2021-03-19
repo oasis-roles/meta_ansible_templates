9db071abb83aa8d05d5dbe408123b6fb77e586e1
Gregory Hellings <greg.hellings@gmail.com>
2021-03-19 11:23:33 -0500

    Workability updates
    setup.sh now does a "git init" if there isn't a .git dir
    Moved .gitignore files from roles to collection
    Removed role-specific .git files

--- collection/setup.sh
+++ collection/setup.sh
@@ -3,6 +3,9 @@ me="$(readlink -f "${0}")"
 home="$(dirname "${me}")"
 
 cd "${home}"
+if [ ! -d ".git" ]; then
+	git init .
+fi
 git submodule add https://github.com/oasis-roles/meta_test tests
 
 rm "${me}"
